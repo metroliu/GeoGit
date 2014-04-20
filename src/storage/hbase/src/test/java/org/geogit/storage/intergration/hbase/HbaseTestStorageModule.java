a1cf5faee28adc4b504d09f60ac3e865885349a3
bestsonny <mybestsonny@gmail.com>
2014-04-20 09:49:49 +0800

    Add hbase conficts,logOp performance,RevTreeBuilder test

--- src/storage/hbase/src/test/java/org/geogit/storage/intergration/hbase/HbaseTestStorageModule.java
+++ src/storage/hbase/src/test/java/org/geogit/storage/intergration/hbase/HbaseTestStorageModule.java
@@ -1,7 +1,9 @@
 package org.geogit.storage.intergration.hbase;
 
 import org.geogit.storage.ObjectDatabase;
+import org.geogit.storage.StagingDatabase;
 import org.geogit.storage.hbase.HBaseObjectDatabase;
+import org.geogit.storage.hbase.HBbaseStagingDatabase;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.Scopes;
@@ -13,6 +15,8 @@ public class HbaseTestStorageModule extends AbstractModule {
 		// TODO Auto-generated method stub
 		// bind(HConnection.class).in(Scopes.SINGLETON);
 		 bind(ObjectDatabase.class).to(HBaseObjectDatabase.class).in(
+	                Scopes.SINGLETON);
+		 bind(StagingDatabase.class).to(HBbaseStagingDatabase.class).in(
 	                Scopes.SINGLETON);
 	}
 
